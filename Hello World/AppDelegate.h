ef2403795aab051459e2f3367d09717d39bfb6a0
cunknown <48595746+anonymous14725@users.noreply.github.com>
2022-09-14 15:15:58 +0430

    App Delegate

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Cunknown on 7/28/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
