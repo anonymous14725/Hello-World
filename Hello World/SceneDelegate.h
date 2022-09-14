7f143327515134e30549cc335c9b84a286502fcd
cunknown <48595746+anonymous14725@users.noreply.github.com>
2022-09-14 15:19:14 +0430

    Scene Delegate

---
+++ Hello World/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Hello World
+//
+//  Created by Cunknown on 7/28/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
